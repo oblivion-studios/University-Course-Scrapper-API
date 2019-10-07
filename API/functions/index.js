ef4e3156ac50bafa6a7c7d951a0ea231a4530409
kisraa <sirakberhane2979@gmail.com>
2019-10-07 19:40:35 -0400

    Added react website

--- API/functions/index.js
+++ API/functions/index.js
@@ -12,7 +12,7 @@ app.use(cors({
     optionsSuccessStatus: 200,
     origin: '*'
 }));
-
+app.use(express.json());
 app.options('*', cors());
 
 app.get('/', apicache.middleware('24 hours'), function (request, response) {
@@ -22,39 +22,81 @@ app.get('/', apicache.middleware('24 hours'), function (request, response) {
         'Content-Type': 'application/json'
     })
 
-    var home = {
-        api: []
-    }
+    asyncHandler(async () => {
+        var home = {
+            api: []
+        }
 
-    var status = {}
-    status.name = "University Course REST API"
-    status.api_version = "1.0.0"
-    var newDate = new Date();
-    var time = dateFormat(newDate.setHours(newDate.getHours() + 24), "yyyy-mm-dd h:MM:ss Z");
-    status.expires = time
-    home.api.push(status)
+        var status = {}
+        status.name = "University Course REST API"
+        status.api_version = "1.0.0"
+        var newDate = new Date();
+        var time = dateFormat(newDate.setHours(newDate.getHours() + 24), "yyyy-mm-dd h:MM:ss Z");
+        status.expires = time
+        home.api.push(status)
 
-    response.end(JSON.stringify(home))
+        response.end(JSON.stringify(home)) 
+    })();   
 })
 
 app.get('/undergraduate', apicache.middleware('24 hours'), function (request, response) {
     var path = require('path');
 
-    var semester = request.param('semester').toLowerCase();
-    var year = request.param('year');
-    var course = request.param('course').toUpperCase();
-   
-    response.sendFile(path.join(__dirname, 'data/Undergraduate/' + semester + year + '/JSON/', course + '.json'));
+    asyncHandler(async () => {
+        var semester = request.param('semester').toLowerCase();
+        var year = request.param('year');
+        var course = request.param('course').toUpperCase();
+        response.sendFile(path.join(__dirname, 'data/undergraduate/' + semester + year + '/JSON/', course + '.json'), {headers: {'Content-Type': 'application/json'}});
+    })();  
 })
 
 app.get('/graduate', apicache.middleware('24 hours'), function (request, response) {
     var path = require('path');
 
-    var semester = request.param('semester').toLowerCase();
-    var year = request.param('year');
-    var course = request.param('course').toUpperCase();
-   
-    response.sendFile(path.join(__dirname, 'data/Graduate/' + semester + year + '/JSON/', course + '.json'));
+    asyncHandler(async () => {
+        var semester = request.param('semester').toLowerCase();
+        var year = request.param('year');
+        var course = request.param('course').toUpperCase();
+        response.sendFile(path.join(__dirname, 'data/graduate/' + semester + year + '/JSON/', course + '.json'), {headers: {'Content-Type': 'application/json'}});
+    })();
+})
+
+app.get('/search', apicache.middleware('5 minutes'), function (req, res) {
+    var Fuse = require('fuse.js');
+
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    })
+
+    asyncHandler(async () => {
+        var level = req.param('level').toLowerCase();
+        var searchKeyword = req.param('keyword');
+        var semester = req.param('semester').toLowerCase();
+        var year = req.param('year');
+        var course = req.param('course').toUpperCase();
+        var list;
+
+        list = JSON.parse(require('fs').readFileSync('data/' + level + '/' + semester + year + '/JSON/' + course + '.json', 'utf8'));
+
+        var options = {
+            shouldSort: true,
+            threshold: 0.6,
+            location: 0,
+            distance: 100,
+            maxPatternLength: 32,
+            minMatchCharLength: 1,
+            keys: [
+                "Title",
+                "Course Number",
+                "Instructor Name",
+                "CRN"
+            ]
+        };
+
+        var fuse = new Fuse(list, options);
+        var result = fuse.search(searchKeyword);
+        res.end(JSON.stringify(result));
+    })();
 })
 
 // Package the entire app routes and export it for Firebase Function Support
